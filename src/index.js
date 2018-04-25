62e41ddf47e3934e975ee06092220111d6f68ec1
samskarvan <samskarvan@gmail.com>
2018-04-24 17:29:08 -0700

    react_intro - Sam Skarvan

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+function luckyNumber(){
+    return Math.floor(Math.random()*1000)+1;
+}
+
+function greeting(user){
+    return <div className="container">
+        <h1>Hey there, {user.name}</h1>
+        <h2 className ="text-muted">Lucky Number: {user.luckyNumber}</h2>
+    </div>
+}
+
+const user = {
+    name: 'Herald',
+    luckyNumber: luckyNumber()
+};
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(user),
     document.getElementById('root')
 );
