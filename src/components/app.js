c4c1f6c96ac94501baf5c0406c63b6fac6aea369
samskarvan <samskarvan@gmail.com>
2018-04-30 10:31:43 -0700

    functional components prototype - Sam Skarvan

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,29 @@
+import React from 'react';
+import Table from './table';
 
+export default()=>{
+    return( 
+    <div className="container">
+    <h1>Student Grade Table</h1>
+    <Table data = {students}/>
+    </div>
+    )
+}
 
+const students =[
+    {
+        name: 'John',
+        course: 'math',
+        grade: 98
+    },
+    {
+        name: 'Katy',
+        course: 'science',
+        grade: 98
+    },
+    {
+        name: 'Margaret',
+        course: 'english',
+        grade: 98
+    }
+];
