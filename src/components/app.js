3e5064d4d7c9498948119ad243083351b6616655
samskarvan <samskarvan@gmail.com>
2018-05-14 11:33:38 -0700

    started redux clock

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,12 @@
+import React from 'react';
+
+
+const App = ()=>(
+        <div className="container">
+            <h1 className="text-center">Any text you want</h1>
+        </div>
+    );
+
+export default App;
 
 
---
+++ src/reducers/clock_reducer.js
@@ -0,0 +1,12 @@
+
+
+const DEFAULT_STATE = {
+    time: null
+};
+
+export default (state= DEFAULT_STATE, action)=>{
+    switch(action.type){
+        default:
+            return state;
+    }
+}
