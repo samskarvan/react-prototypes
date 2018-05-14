85b870bbe8bee08ab2d7d8fa1eb31bc3262e622e
samskarvan <samskarvan@gmail.com>
2018-05-14 10:22:24 -0700

    completed outing

--- src/components/nav.js
+++ src/components/nav.js
@@ -6,7 +6,7 @@ export default props=>(
     <ul className="nav nav-tabs mt-3">
         <li className="nav-item"><NavLink activeClassName="active selected" exact to="/" className="nav-link">Bienvenido</NavLink></li>
         <li className="nav-item"><NavLink activeClassName="active selected" to="/macarrones" className="nav-link">Macarrones</NavLink></li>
-        <li className="nav-item"><NavLink activeClassName="active selected" to="/contactanos" className="nav-link">Contactanos</NavLink></li>
-        <li className="nav-item"><NavLink activeClassName="active selected" to="/regalos-y-fiestas" className="nav-link">Regalos y Fiestas</NavLink></li>
+        <li className="nav-item"><NavLink activeClassName="active selected" to="/contact" className="nav-link">Contactanos</NavLink></li>
+        <li className="nav-item"><NavLink activeClassName="active selected" to="/gifts-and-parties" className="nav-link">Regalos y Fiestas</NavLink></li>
     </ul>
 )
